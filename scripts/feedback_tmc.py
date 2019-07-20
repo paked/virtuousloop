7f477e7c13b1ffc0f1cb72659d8cd96839dd080e
Chris Browne <chris.browne@anu.edu.au>
2019-07-20 19:06:19 +1000

    try quotes

--- scripts/feedback_tmc.py
+++ scripts/feedback_tmc.py
@@ -140,7 +140,7 @@ def feedback_tmc():
             
 
         # use the anu_cecs.latex template
-        pdoc_args = ['--template=./includes/pdf/anu_cecs.latex --pdf-engine=xelatex']
+        pdoc_args = ['--template=./includes/pdf/anu_cecs.latex', '--pdf-engine=xelatex']
         # convert to pdf
         output = pypandoc.convert_file(this_out, to='pdf', format='md', outputfile=this_pdf, extra_args=pdoc_args)
 
